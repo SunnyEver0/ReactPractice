6d69f36d2fa55b201248997f317b015f9f616914
Russell.H.Sun <russell.h.sun@newegg.com>
2018-09-12 18:19:07 +0800

    add mobx

--- react/mobx-practice/src/App.js
+++ react/mobx-practice/src/App.js
@@ -1,14 +1,49 @@
 import "./App.css";
 
 import React, { Component } from "react";
-import { computed, observable } from "mobx";
+import { autorun, observable } from "mobx";
 
-import logo from "./logo.svg";
 import { observer } from "mobx-react";
 
-var appState = observable({
-  timer: 0
+var todoStore = observable({
+  /* 一些观察的状态 */
+  todos: [],
+
+  /* 推导值 */
+  get completedCount() {
+    return this.todos.filter(todo => todo.completed).length;
+  }
 });
+
+/* 观察状态改变的函数 */
+autorun(function() {
+  console.log(
+    "Completed %d of %d items",
+    todoStore.completedCount,
+    todoStore.todos.length
+  );
+});
+
+/* ..以及一些改变状态的动作 */
+todoStore.todos[0] = {
+  title: "Take a walk",
+  completed: false
+};
+// -> 同步打印 'Completed 0 of 1 items'
+
+todoStore.todos[0].completed = true;
+// -> 同步打印 'Completed 1 of 1 items'
+
+let appState = observable({
+  timer: 0,
+  resetTimer: () => {
+    appState.timer = 0;
+  }
+});
+
+setInterval(() => {
+  appState.timer += 1;
+}, 1000);
 
 @observer
 class TimerView extends React.Component {
@@ -25,20 +60,9 @@ class TimerView extends React.Component {
   }
 }
 
-
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return <TimerView appState={appState} />;
   }
 }
 
